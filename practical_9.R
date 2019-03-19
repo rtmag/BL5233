0678ef23001efd868f30abfca231323d62ef42fc
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-19 12:01:07 +0800

    Update practical_9.R

--- practical_9.R
+++ practical_9.R
@@ -7,10 +7,10 @@ xyplot(follicles ~ Time, data, grid = TRUE)
 library(nlme )
 
 lme1 <- lme(follicles ~ Time * Mare, data, random =~1|Mare)
+lme2 <- lme(follicles ~ 1 * Mare, data, random =~1|Mare)
 
 plot(ACF(lme1),alpha=0.05)
 
-cs1 = corARMA(c(.3), p = 0, q = 0)
 cs2 = corARMA(c(.3), p = 1, q = 0)
 cs3 = corARMA(c(.3), p = 0, q = 1)
 cs4 = corARMA(c(.3,-.3), p = 0, q = 2)
@@ -22,7 +22,7 @@ cs9 = corARMA(c(.3,-.3,.2,-.2), p = 2, q = 2)
 
 arma1 <- lme(follicles ~ Time * Mare, data = data, correlation = cs1, random =~1|Mare)
 arma2 <- lme(follicles ~ Time * Mare, data = data, correlation = cs2, random =~1|Mare)
-arma3 <- lme(follicles ~ Time * Mare, data = data, correlation = cs3, random =~1|Mare)
+arma3 <- lme(follicles ~ Time * Mare, data = data, correlation = cs6, random =~1|Mare)
 arma4 <- lme(follicles ~ Time * Mare, data = data, correlation = cs4)
 arma5 <- lme(follicles ~ Time * Mare, data = data, correlation = cs5)
 arma6 <- lme(follicles ~ Time * Mare, data = data, correlation = cs6)
@@ -33,4 +33,17 @@ arma9 <- lme(follicles ~ Time * Mare, data = data, correlation = cs9)
 
 AIC(arma1,arma2,arma3,arma4,arma5,arma6,arma7,arma8,arma9)
 #########################################################
+install.packages("MCMCglmm")
+library(MCMCglmm)
+BTdata = data("BTdata")
+BTped = data("BTped")
 
+m <- MCMCglmm(y ~ (a + b + c)^3,
+                       ~ us(1 + (a + b + c)^3):subject +
+                         us(1 + (a + b    )^2):item,
+               data   = d,
+               family = "categorical",
+               prior  = prior.m3,
+               thin   = 20,
+               burnin = 3000,
+               nitt   = 23000)
