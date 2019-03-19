3f5fe84ae016ed32f66e2c592c08db02091505d3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-19 11:36:50 +0800

    Create practical_9.R

---
+++ practical_9.R
@@ -0,0 +1,36 @@
+data = read.table("Ovary.txt",header=T)
+
+library(lattice)
+
+xyplot(follicles ~ Time, data, grid = TRUE)
+
+library(nlme )
+
+lme1 <- lme(follicles ~ Time * Mare, data, random =~1|Mare)
+
+plot(ACF(lme1),alpha=0.05)
+
+cs1 = corARMA(c(.3), p = 0, q = 0)
+cs2 = corARMA(c(.3), p = 1, q = 0)
+cs3 = corARMA(c(.3), p = 0, q = 1)
+cs4 = corARMA(c(.3,-.3), p = 0, q = 2)
+cs5 = corARMA(c(.3,-.3), p = 2, q = 0)
+cs6 = corARMA(c(.3,-.3), p = 1, q = 1)
+cs7 = corARMA(c(.3,-.3,.2), p = 1, q = 2)
+cs8 = corARMA(c(.3,-.3,.2), p = 2, q = 1)
+cs9 = corARMA(c(.3,-.3,.2,-.2), p = 2, q = 2)
+
+arma1 <- lme(follicles ~ Time * Mare, data = data, correlation = cs1, random =~1|Mare)
+arma2 <- lme(follicles ~ Time * Mare, data = data, correlation = cs2, random =~1|Mare)
+arma3 <- lme(follicles ~ Time * Mare, data = data, correlation = cs3, random =~1|Mare)
+arma4 <- lme(follicles ~ Time * Mare, data = data, correlation = cs4)
+arma5 <- lme(follicles ~ Time * Mare, data = data, correlation = cs5)
+arma6 <- lme(follicles ~ Time * Mare, data = data, correlation = cs6)
+arma7 <- lme(follicles ~ Time * Mare, data = data, correlation = cs7)
+arma8 <- lme(follicles ~ Time * Mare, data = data, correlation = cs8)
+arma9 <- lme(follicles ~ Time * Mare, data = data, correlation = cs9)
+
+
+AIC(arma1,arma2,arma3,arma4,arma5,arma6,arma7,arma8,arma9)
+#########################################################
+
